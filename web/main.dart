2259a657da2cd487f3167b8be7dc9001fd55e6b9
Harry Terkelsen <het@google.com>
2015-11-30 10:18:22 -0800

    add ng2.largetable.interpolation.plain to benchmark results

--- web/main.dart
+++ web/main.dart
@@ -11,7 +11,8 @@ import 'dart:math';
 Iterable _columns = [
   new ChartColumnSpec(label: 'Benchmark', type: ChartColumnSpec.TYPE_STRING),
   new ChartColumnSpec(label: 'js Runtime (ms)'),
-  new ChartColumnSpec(label: 'dart2js Runtime (ms)')
+  new ChartColumnSpec(label: 'dart2js (ssa) Runtime (ms)'),
+  new ChartColumnSpec(label: 'dart2js (cps) Runtime (ms)')
 ];
 
 Set<String> treeBenchmarks = new Set.from(
@@ -25,8 +26,9 @@ Set<String> staticTreeBenchmarks = new Set.from([
 
 Set<String> largetableBenchmarks = new Set.from([
   'ng2.largetable.interpolation',
+  'ng2.largetable.interpolation.plain',
   'ng2.largetable.interpolationAttr',
-  'ng2.largetable.interpolationFn'
+  'ng2.largetable.interpolationFn',
 ]);
 
 Set<String> naiveInfiniteScrollBenchmarks = new Set.from([
@@ -69,11 +71,24 @@ void showMeans() {
   var naiveInfiniteScrollMean =
       showMean('naive_infinite_scroll', naiveInfiniteScrollBenchmarks);
   var totalMean = showMean(
-      'all macro',
+      'ssa vs js',
       treeBenchmarks
           .union(largetableBenchmarks)
           .union(naiveInfiniteScrollBenchmarks)
           .union(staticTreeBenchmarks));
+  var cpsJsMean = showMean(
+      'cps vs js',
+      treeBenchmarks
+          .union(largetableBenchmarks)
+          .union(naiveInfiniteScrollBenchmarks)
+          .union(staticTreeBenchmarks),
+      denom: 3);
+  var cpsSsaMean = showMean(
+      'cps vs ssa',
+      treeBenchmarks
+          .union(largetableBenchmarks)
+          .union(naiveInfiniteScrollBenchmarks)
+          .union(staticTreeBenchmarks), denom: 3, numer: 2);
 
   var meansDiv = querySelector('#means');
   meansDiv.append(new UListElement()
@@ -81,12 +96,15 @@ void showMeans() {
     ..append(new LIElement()..text = staticTreeMean)
     ..append(new LIElement()..text = largeTableMean)
     ..append(new LIElement()..text = naiveInfiniteScrollMean)
-    ..append(new LIElement()..text = totalMean));
+    ..append(new LIElement()..text = totalMean)
+    ..append(new LIElement()..text = cpsJsMean)
+    ..append(new LIElement()..text = cpsSsaMean));
 }
 
-String showMean(String name, Set<String> benchmarks) {
+// XXX: rename denom and numer
+String showMean(String name, Set<String> benchmarks, {int denom: 2, int numer: 1}) {
   var mean = geoMean(
-      rawRows.where((x) => benchmarks.contains(x[0])).map((x) => x[2] / x[1]));
+      rawRows.where((x) => benchmarks.contains(x[0])).map((x) => x[denom] / x[numer]));
   return 'Average performance on $name benchmarks: $mean';
 }
 
@@ -107,7 +125,7 @@ void renderChart(String id, Set<String> benchmarks) {
   var columns = new ObservableList.from(_columns);
   var data = new ChartData(columns, rows);
   var seriesList = new ObservableList.from([
-    new ChartSeries('Default', new ObservableList.from([1, 2]),
+    new ChartSeries('Default', new ObservableList.from([1, 2, 3]),
         new BarChartRenderer(alwaysAnimate: true))
   ]);
   var config = new ChartConfig(seriesList, [0]);
@@ -119,16 +137,20 @@ void renderChart(String id, Set<String> benchmarks) {
 
 List parseToRows(Map json) {
   var jsScores = {};
-  var dartScores = {};
+  var ssaScores = {};
+  var cpsScores = {};
   for (var result in json['js']) {
     jsScores[result['benchmark']] = result['score'];
   }
-  for (var result in json['dart2js']) {
-    dartScores[result['benchmark']] = result['score'];
+  for (var result in json['ssa']) {
+    ssaScores[result['benchmark']] = result['score'];
+  }
+  for (var result in json['cps']) {
+    cpsScores[result['benchmark']] = result['score'];
   }
   var rows = [];
   for (var benchmark in jsScores.keys) {
-    rows.add([benchmark, jsScores[benchmark], dartScores[benchmark]]);
+    rows.add([benchmark, jsScores[benchmark], ssaScores[benchmark], cpsScores[benchmark]]);
   }
   return rows;
 }
